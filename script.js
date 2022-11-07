391ea62094b2ee7c1121b31f142011e41fc735ab
bibekbee <37708553+bibekbee@users.noreply.github.com>
2022-11-07 18:26:01 +0545

    new files
    new

---
+++ script.js
@@ -0,0 +1,39 @@
+const carousel = document.querySelector(".carousel");
+const firstImg = carousel.querySelectorAll("img")[0];
+const arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 15; //getting first image width and adding 14 margin value 
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        //if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth
+    })
+})
+
+const dragStart = (e) => {
+    //updating global variable value on mouse down
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to the mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.scrollLeft = e.pageX;
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+} 
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
