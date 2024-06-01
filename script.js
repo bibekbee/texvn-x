38f72dfebabb201e181cd579cd05542658c0983f
bibekbee <bibekbee23@gmail.com>
2024-06-01 12:34:45 +0545

    Github Projects Laravel

--- script.js
+++ script.js
@@ -38,11 +38,26 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 
-window.onload = () => {
-    const link = document.getElementById('C-link');
-    if(window.screen.width < 600){
-        link.innerHTML = 'link';
-    }else{
-        link.innerHTML = 'https://github.com/bibekbee/C_passManager';
+function loadall(links){
+    const bucket = [];
+    bucket[0] = [];
+    bucket[1] = [];
+    links.forEach(e => {
+        bucket[0].push(`${e.id}`);
+        bucket[1].push(`${e.href}`);
+    });
+    return bucket;
+}
+
+window.onload =  () => {
+    const link = document.querySelectorAll('.C-link');
+    const results = loadall(link);
+    for(let i in results){
+        let current_link = document.getElementById(results[0][i]);
+        if(window.screen.width < 600){
+            current_link.innerHTML = 'link';
+        }else{
+            current_link.innerHTML = results[1][i];
+        }
     }
 }
