c37669c78217c487d1039b68d90bc79b20460974
bibekbee <bibekbee23@gmail.com>
2024-06-08 09:31:29 +0545

    Project OnlyPHP added

---
+++ prjScript.js
@@ -0,0 +1,25 @@
+function loadall(links){
+    const bucket = [];
+    bucket[0] = [];
+    bucket[1] = [];
+    links.forEach(e => {
+        bucket[0].push(`${e.id}`);
+        bucket[1].push(`${e.href}`);
+    });
+    return bucket;
+}
+
+window.onload =  () => {
+    const link = document.querySelectorAll('.C-link');
+    const results = loadall(link);
+    console.log(results);
+    for(let i in results[0]){
+        let current_link = document.getElementById(results[0][i]);
+        console.log(current_link);
+        if(window.screen.width < 600){
+            current_link.innerHTML = 'link';
+        }else{
+            current_link.innerHTML = results[1][i];
+        }
+    }
+}
