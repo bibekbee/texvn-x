4cc36ff2ed978998d9a0a0d081ef6139eed93567
bibekbee <bibekbee23@gmail.com>
2024-06-18 16:09:32 +0545

    Added one more framework

--- script.js
+++ script.js
@@ -38,26 +38,3 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 
-function loadall(links){
-    const bucket = [];
-    bucket[0] = [];
-    bucket[1] = [];
-    links.forEach(e => {
-        bucket[0].push(`${e.id}`);
-        bucket[1].push(`${e.href}`);
-    });
-    return bucket;
-}
-
-window.onload =  () => {
-    const link = document.querySelectorAll('.C-link');
-    const results = loadall(link);
-    for(let i in results){
-        let current_link = document.getElementById(results[0][i]);
-        if(window.screen.width < 600){
-            current_link.innerHTML = 'link';
-        }else{
-            current_link.innerHTML = results[1][i];
-        }
-    }
-}
